cf782ebc019ebc39d27b1a203ac418fbc2b837b1
yhoogstrate <y.hoogstrate@gmail.com>
2018-09-05 12:20:07 +0200

    flake

--- blc/__init__.py
+++ blc/__init__.py
@@ -1,12 +1,9 @@
-ll
 #!/usr/bin/env python
 # *- coding: utf-8 -*-
 # vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=79:
 
 """[License: GNU General Public License v3 (GPLv3)]
-
 """
-
 
 __version_info__ = ('2', '0', '0')
 __version__ = '.'.join(__version_info__) if (len(__version_info__) == 3) else '.'.join(__version_info__[0:3]) + "-" + __version_info__[3]
@@ -14,4 +11,3 @@ __author__ = 'Youri Hoogstrate'
 __homepage__ = 'https://github.com/yhoogstrate/bam-lorenz-coverage'
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __license_notice__ = 'License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.'
-
