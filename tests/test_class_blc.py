002c75b8ae7eb0efbe2bc020f9e5b0a79036c6b3
yhoogstrate <y.hoogstrate@gmail.com>
2018-09-11 10:26:34 +0200

    more tests for bam to stats parsing and conversion to coverage idx

--- tests/test_class_blc.py
+++ tests/test_class_blc.py
@@ -75,6 +75,67 @@ class TestIntronicBreakDetection(unittest.TestCase):
         
         self.assertDictEqual(cc, {'minimum_coverage_depth': [0, 1, 2, 3], 'percentage_genome_covered': [100.0, 60.0, 40.0, 20.0]})
 
+    def test_004_test_splice_junction(self):
+        test_id = 'blc_004'
+
+        input_file_sam = TEST_DIR + "test_" + test_id + ".sam"
+        input_file_bam = T_TEST_DIR + "test_" + test_id + ".bam"
+
+        sam_to_sorted_bam(input_file_sam, input_file_bam)
+
+        b = bamlorenzcoverage()
+        idx = b.bam_file_to_idx(input_file_bam)
+
+        # print(idx, file=sys.stderr)
+        # denote that it only considers the (size of the) sequences described in the SAM header
+        self.assertDictEqual(idx, {0: 392, 1: 108})
+
+    def test_005_deletion(self):
+        test_id = 'blc_005'
+
+        input_file_sam = TEST_DIR + "test_" + test_id + ".sam"
+        input_file_bam = T_TEST_DIR + "test_" + test_id + ".bam"
+
+        sam_to_sorted_bam(input_file_sam, input_file_bam)
+
+        b = bamlorenzcoverage()
+        idx = b.bam_file_to_idx(input_file_bam)
+
+        # print(idx, file=sys.stderr)
+        # denote that it only considers the (size of the) sequences described in the SAM header
+        self.assertDictEqual(idx, {0: 392, 1: 108})
+
+    def test_006_insertion(self):
+        test_id = 'blc_006'
+
+        input_file_sam = TEST_DIR + "test_" + test_id + ".sam"
+        input_file_bam = T_TEST_DIR + "test_" + test_id + ".bam"
+
+        sam_to_sorted_bam(input_file_sam, input_file_bam)
+
+        b = bamlorenzcoverage()
+        idx = b.bam_file_to_idx(input_file_bam)
+
+        # print(idx, file=sys.stderr)
+        # denote that it only considers the (size of the) sequences described in the SAM header
+        self.assertDictEqual(idx, {0: 400, 1: 100})
+
+    def test_007_stacking(self):
+        test_id = 'blc_007'
+
+        input_file_sam = TEST_DIR + "test_" + test_id + ".sam"
+        input_file_bam = T_TEST_DIR + "test_" + test_id + ".bam"
+
+        sam_to_sorted_bam(input_file_sam, input_file_bam)
+
+        b = bamlorenzcoverage()
+        idx = b.bam_file_to_idx(input_file_bam)
+
+        # print(idx, file=sys.stderr)
+        # denote that it only considers the (size of the) sequences described in the SAM header
+        self.assertDictEqual(idx, {0: 372, 1: 48, 2: 80})
+
+
 
 if __name__ == '__main__':
     main()
